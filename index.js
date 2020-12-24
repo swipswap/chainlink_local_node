3084729ed83e8bf366488c0b5358202cc75e4b59
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2020-12-24 17:17:51 +0700

    refactored the deployment script

--- index.js
+++ index.js
@@ -40,14 +40,14 @@ const getAddressFunder = (provider, startingIndex=2) => {
 const deployChainlinkToken = async (signer) => {
     const factory = new ethers.ContractFactory(tokenABI, tokenBytecode, signer)
     const contract = await factory.deploy()
-    contract.deployTransaction.wait()
+    await contract.deployTransaction.wait()
     return contract
 }
 
 const setupChainlinkOracle = async (signer, linkToken, nodeAddress) => {
     const factory = new ethers.ContractFactory(oracleABI, oracleBytecode, signer)
     const contract = await factory.deploy(linkToken)
-    contract.deployTransaction.wait()
+    await contract.deployTransaction.wait()
     await contract.setFulfillmentPermission(nodeAddress, true)
     return contract
 }
@@ -86,37 +86,61 @@ const getNodeAddress = async (delay=5) =>{
 const main = async (callbackFunction=()=>{}) => {
     let _store = {}
     await sleep(5)
-    const provider = await connectAndGetProvider()
-    const knownSigner = await getKnownSigner(provider)
-    const fundAddress = getAddressFunder(provider)
-    await fundAddress(knownSigner.getAddress())
-    await fundAddress(config.testAddress)
 
-    const chainlinkToken = await deployChainlinkToken(knownSigner)
-
-    const nodeAddress = await getNodeAddress()
-    if(nodeAddress === ""){
+    _store.provider = await connectAndGetProvider()
+    _store.signer = await getKnownSigner(_store.provider)
+    
+    _store.nodeAddress = await getNodeAddress()
+    if(_store.nodeAddress === "") {
         return
     }
-    await fundAddress(nodeAddress)
+   
+    const fundAddress = getAddressFunder(_store.provider)
 
-    await chainlinkToken.transfer(nodeAddress, ethers.utils.parseEther("10000"))
-    await chainlinkToken.transfer(config.testAddress, ethers.utils.parseEther("1000"))
+    const signerBalance = await _store.signer.getBalance()
+    if(Number(signerBalance) === 0) {
+        await fundAddress(await _store.signer.getAddress())
+        console.log(`Funded deploying account: ${await _store.signer.getAddress()} with ETH`)
+    }
+
+    // If an external test address isn't provided in the config.json file, use the default from mnemonic
+    if(!config.testAddress) {
+        config.testAddress = _store.signer.address
+    }
+
+    const testAddressBalance = await _store.provider.getBalance(config.testAddress)
+
+
+    if(_store.signer.address !== config.testAddress && Number(testAddressBalance) === 0) {
+        // fund the test address
+        await fundAddress(config.testAddress)
+        console.log(`Funded test address: ${config.testAddress} with ETH`)
+    }
+        
+    const nodeAddressBalance = await _store.provider.getBalance(_store.nodeAddress)
+    if(Number(nodeAddressBalance) === 0) {
+        await fundAddress(_store.nodeAddress)
+        console.log(`Funded node address: ${_store.nodeAddress} with ETH`)
+    }
+
+    _store.chainlinkToken = await deployChainlinkToken(_store.signer)
+
+    await _store.chainlinkToken.transfer(_store.nodeAddress, ethers.utils.parseEther("10000"))
+    await _store.chainlinkToken.transfer(config.testAddress, ethers.utils.parseEther("1000"))
     
-    const chainlinOracle = await setupChainlinkOracle(knownSigner, chainlinkToken.address, nodeAddress)
-
-    _store = {..._store, chainlinkToken, provider, knownSigner, chainlinOracle, nodeAddress}
+    _store.chainlinkOracle = await setupChainlinkOracle(_store.signer, _store.chainlinkToken.address, _store.nodeAddress)
 
     callbackFunction(_store)
 }
 
 
 
-const callbackFunction = (store) => {
+const callbackFunction = async (store) => {
     console.log({
+        deployedBy: await store.signer.getAddress(),
         chainlinkToken: store.chainlinkToken.address,
-        chainlinOracle: store.chainlinOracle.address,
-        nodeAddress:    store.nodeAddress
+        chainlinkOracle: store.chainlinkOracle.address,
+        nodeAddress: store.nodeAddress
     })
 }
 
